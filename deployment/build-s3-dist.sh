79a4c42877aadda9a1e680204598c9ce26c5852e
Robin Bowes <robin.bowes@yo61.com>
2017-11-02 17:04:31 +0000

    Ignore deployment/dist directory

--- deployment/build-s3-dist.sh
+++ deployment/build-s3-dist.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # strict mode
-set -euo pipefail
+set -euxo pipefail
 
 # This assumes all of the OS-level configuration has been completed and git repo has already been cloned
 #
@@ -35,72 +35,60 @@ if [ -z "$1" ]; then
 fi
 
 # Build source
-echo "Staring to build distribution"
-echo "export deployment_dir=$(pwd)"
+printf "Starting to build distribution"
 deployment_dir=$(pwd)
 export deployment_dir
-echo "mkdir -p dist"
 mkdir -p dist
-echo "cp -f serverless-image-handler.template dist"
 cp -f serverless-image-handler.template dist
-echo "Updating code source bucket in template with $1"
+
+printf "Updating code source bucket in template with %s" "$1"
 replace="s/%%BUCKET_NAME%%/$1/g"
-echo "sed -i'' -e $replace dist/serverless-image-handler.template"
 sed -i'' -e "$replace" dist/serverless-image-handler.template
-echo "Creating UI ZIP file"
-cd "$deployment_dir/../source/ui" || exit 1
+
+printf "Creating UI ZIP file"
+cd "$deployment_dir/../source/ui"
 zip -q -r9 "$deployment_dir/dist/serverless-image-handler-ui.zip" ./*
-echo "Building custom resource package ZIP file"
-cd "$deployment_dir/dist" || exit 1
+
+printf "Building custom resource package ZIP file"
+cd "$deployment_dir/dist"
 pwd
-echo "virtualenv env"
 virtualenv env
-echo "VIRTUAL_ENV_DISABLE_PROMPT=true source env/bin/activate"
 VIRTUAL_ENV_DISABLE_PROMPT=true source env/bin/activate
-echo "pip install $deployment_dir/../source/image-handler-custom-resource/. --target=$deployment_dir/dist/env/lib/python2.7/site-packages/"
 pip install "$deployment_dir/../source/image-handler-custom-resource/." --target="$deployment_dir/dist/env/lib/python2.7/site-packages/"
-cd "$deployment_dir/dist/env/lib/python2.7/site-packages/" || exit 1
+cd "$deployment_dir/dist/env/lib/python2.7/site-packages/"
 zip -r9 "$deployment_dir/dist/serverless-image-handler-custom-resource.zip" ./*
-cd "$deployment_dir/dist" || exit 1
+cd "$deployment_dir/dist"
 zip -q -d serverless-image-handler-custom-resource.zip pip*
 zip -q -d serverless-image-handler-custom-resource.zip easy*
 rm -rf env
-echo "Building Image Handler package ZIP file"
-cd "$deployment_dir/dist" || exit 1
+
+printf "Building Image Handler package ZIP file"
+cd "$deployment_dir/dist"
 pwd
-echo "virtualenv env"
 virtualenv env
-echo "VIRTUAL_ENV_DISABLE_PROMPT=true source env/bin/activate"
 VIRTUAL_ENV_DISABLE_PROMPT=true source env/bin/activate
-cd ../.. || exit 1
+cd ../..
 pwd
-echo "pip install source/image-handler/. --target=$VIRTUAL_ENV/lib/python2.7/site-packages/"
 pip install source/image-handler/. --target="$VIRTUAL_ENV/lib/python2.7/site-packages/"
-echo "pip install -r source/image-handler/requirements.txt --target=$VIRTUAL_ENV/lib/python2.7/site-packages/"
 pip install -r source/image-handler/requirements.txt --target="$VIRTUAL_ENV/lib/python2.7/site-packages/"
-cd "$VIRTUAL_ENV" || exit 1
+cd "$VIRTUAL_ENV"
 pwd
-echo "git clone git://github.com/pornel/pngquant.git pngquant_s"
 git clone https://github.com/pornel/pngquant.git pngquant_s
-cd pngquant_s || exit 1
+cd pngquant_s
 pwd
-echo "./configure --enable-static --disable-shared"
 ./configure --enable-static --disable-shared
-echo "make"
 make
-echo "cp pngquant $VIRTUAL_ENV"
 cp -f pngquant "$VIRTUAL_ENV"
-cd "$VIRTUAL_ENV/lib/python2.7/site-packages" || exit 1
+cd "$VIRTUAL_ENV/lib/python2.7/site-packages"
 pwd
-echo "zip -q -r9 $VIRTUAL_ENV/../serverless-image-handler.zip *"
 zip -q -r9 "$VIRTUAL_ENV/../serverless-image-handler.zip" ./*
-cd "$VIRTUAL_ENV" || exit 1
+cd "$VIRTUAL_ENV"
 pwd
-echo "zip -q -g $VIRTUAL_ENV/../serverless-image-handler.zip pngquant"
 zip -q -g "$VIRTUAL_ENV/../serverless-image-handler.zip" pngquant
 cd ..
 zip -q -d serverless-image-handler.zip pip*
 zip -q -d serverless-image-handler.zip easy*
-echo "Clean up build material"
+
+printf "Clean up build material"
 rm -rf "$VIRTUAL_ENV"
-echo "Completed building distribution"
+printf "Completed building distribution"
