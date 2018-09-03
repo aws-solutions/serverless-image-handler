6bbc0370fd86cbb33713be3ac2dd80b175566ace
Levi Wilson <levi@leviwilson.com>
2018-09-03 09:36:42 -0400

    specify pip._internal.req vs. pip.req
    pip.req was removed in pip >= 10

--- source/image-handler-custom-resource/setup.py
+++ source/image-handler-custom-resource/setup.py
@@ -1,10 +1,7 @@
 # coding: utf-8
 
 from setuptools import setup, find_packages
-try: # for pip >= 10
-    from pip._internal.req import parse_requirements
-except ImportError: # for pip <= 9.0.3
-    from pip.req import parse_requirements
+from pip._internal.req import parse_requirements
 
 setup(
     name='image_handler_custom_resource',
