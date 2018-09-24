604306df9f4738de2a023b03da7f3aa953650096
Charles Hepner <charleshepner@gmail.com>
2018-09-24 11:16:51 -0400

    add branch name to artifact

--- buildme.sh
+++ buildme.sh
@@ -3,6 +3,9 @@
 set -eu
 export SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd ${SCRIPT_DIR}
+
+GIT_SHA=$(git rev-parse --short HEAD)
+GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 OUTDIR=${SCRIPT_DIR}/deployment/dist
 ARTIFACT=serverless-image-handler
 TIMESTAMP=$(date +%Y%m%d%H%M%S)
@@ -12,4 +15,4 @@ docker build -t serverless-image-handler .
 
 docker run --rm -v `pwd`:/lambda serverless-image-handler
 
-aws s3 cp ${OUTDIR}/${ARTIFACT}.zip s3://traverse-lambda-artifacts/${ARTIFACT}-${VERSION}.zip
+aws s3 cp ${OUTDIR}/${ARTIFACT}.zip s3://traverse-lambda-artifacts/${ARTIFACT}-${GIT_BRANCH}-${VERSION}.zip
