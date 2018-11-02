334d9af8e01f2da5303f005d50a3ebc297330087
Chris Merrett <chris@steamhaus.co.uk>
2018-11-02 14:31:16 +0000

    Strip whitespace from CORS origin list members, tweak single CORS origin regex matcher
    Signed-off-by: William Griffiths <william.griffiths@thebookpeople.co.uk>

--- source/image-handler/lambda_function.py
+++ source/image-handler/lambda_function.py
@@ -73,19 +73,20 @@ def response_formater(status_code='400',
 
     if str(os.environ.get('ENABLE_CORS')).upper() == "YES":
         # If CORS_ORIGIN contains no commas (single origin)
-        if re.match(r"^[A-Za-z0-9.-]+$", str(os.environ.get('CORS_ORIGIN'))):
+        if re.match(r"^http[://A-Za-z0-9.-]+$", str(os.environ.get('CORS_ORIGIN'))):
             api_response['headers']['Access-Control-Allow-Origin'] = os.environ.get('CORS_ORIGIN')
         # If CORS_ORIGIN contains commas (multiple origins)
         if re.match(r"[,]", str(os.environ.get('CORS_ORIGIN'))):
             origins = os.environ.get('CORS_ORIGIN')).split(",")
             for i in range(len(origins)) :
-                if original_request['headers'].get('Origin') == origins[i]:
-                    api_response['headers']['Access-Control-Allow-Origin'] = origins[i]
-                    logging.debug('api origin match: %s' % origins[i])
+                current_origin = origins[i].strip()
+                if original_request['headers'].get('Origin') == current_origin:
+                    api_response['headers']['Access-Control-Allow-Origin'] = current_origin
+                    logging.debug('api origin match: %s' % current_origin)
                     break
             else
                 # Set the first origin header in the list, which will cause a CORS mismatch failure client-side
-                api_response['headers']['Access-Control-Allow-Origin'] = origins[0]
+                api_response['headers']['Access-Control-Allow-Origin'] = origins[0].strip()
                 logging.debug('api origin mismatch: %s' % original_request['headers'].get('Origin'))
 
     if int(status_code) != 200:
