0389a66a70a893f515b41441a608d12de23d6855
Jason Rae <jasonrae63@gmail.com>
2020-04-14 00:18:34 -0700

    Reduce the amount of logging and make the handler async - tile first

--- source/custom-resource/index.js
+++ source/custom-resource/index.js
@@ -13,7 +13,7 @@
 
 'use strict';
 
-console.log('Loading function');
+// console.log('Loading function');
 
 const AWS = require('aws-sdk');
 const fs = require('fs');
@@ -22,29 +22,22 @@ const S3 = require('aws-sdk/clients/s3');
 const s3 = new S3();
 const sharp = require('sharp');
 const https = require('https');
-const url = require('url');
+const url = require('url').URL;
 
 /**
  * Request handler.
  */
-exports.handler = (event) => {
-    console.log('!! logging event', event);
-
+exports.handler = async (event, context, callback) => {
+    // console.log('!! logging event', event);
     if(event.Records[0]['eventSource'] == "aws:sqs") {
-
         const requestBody = JSON.parse(event.Records[0]['body']);
-        console.log(requestBody);
-        var s3Key = requestBody['aws_key'];
-
-        tileImage(process.env.S3_BUCKET, s3Key);
-
-        console.log('tiled image, send ready');
-
+        await tileImage(process.env.S3_BUCKET, requestBody);
         sendResponse(
             requestBody['callback_url'],
             requestBody['callback_token'],
             requestBody['image_number'],
-            'ready');
+            'ready',
+            context);
     }
 };
 
@@ -54,11 +47,10 @@ exports.handler = (event) => {
  * @param {String} key - The key name corresponding to the image.
  * @return {Promise} - The original image or an error.
  */
-let tileImage = async function(bucket, key) {
-    const imagesLocation = key
+let tileImage = async function(bucket, requestBody) {
+    const imagesLocation = requestBody['aws_key']
     const uniq_key = imagesLocation.split('/').pop()
     const tmp_location = '/tmp/' + uniq_key
-
     try {
         const originalImage = await getOriginalImage(bucket, imagesLocation);
         const image = sharp(originalImage);
@@ -66,21 +58,27 @@ let tileImage = async function(bucket, key) {
             layout: 'zoomify'
           }).toFile(tmp_location + 'tiled.dz', function(err, info) {
             if (err) {
-                console.log('err', err);
+                // console.log('err', err);
             } else {
-                console.log('successfully tiled images ' + tmp_location);
-                Promise.all(upload_recursive_dir(tmp_location + 'tiled/', bucket, key + '/tiles', [])).then(function(errs, data) {
-                        if (errs.length) console.log('errors ', errs);// an error occurred
-                        console.log('successfully uploaded tiled images');
+                // console.log('successfully tiled images ' + tmp_location);
+                Promise.all(upload_recursive_dir(tmp_location + 'tiled/', bucket, requestBody['aws_key'] + '/tiles', [])).then(function(errs, data) {
+                        // if (errs.length) console.log('errors ', errs);// an error occurred
+                        // console.log('successfully uploaded tiled images');
                     }).catch(function(exception) {
-                        console.log('caught exception', exception);
+                        console.error('caught exception', exception);
+                        sendResponse(
+                            requestBody['callback_url'],
+                            requestBody['callback_token'],
+                            requestBody['image_number'],
+                            'error');
                     }).finally(function() {
                         deleteFolderRecursive(tmp_location + 'tiled/');
-                        console.log('successfully deleted tmp files');
+                        // console.log('successfully deleted tmp files');
                     });;
             }
         });
     } catch(err) {
+        console.error('caught exception', err);
         return Promise.reject({
             status: 500,
             code: err.code,
@@ -99,7 +97,7 @@ let tileImage = async function(bucket, key) {
 let getOriginalImage = async function(bucket, imagesLocation) {
     let images = await getImageObjects(bucket, imagesLocation);
     let originalObject = images.find(isOriginal);
-    console.log('originalObject filename', originalObject.Key);
+    // console.log('originalObject filename', originalObject.Key);
     return downloadImage(bucket, originalObject.Key);
 }
 
@@ -153,7 +151,7 @@ let upload_recursive_dir = function(base_tmpdir, destS3Bucket, s3_key, promises)
             promises = upload_recursive_dir(local_temp_path + '/', destS3Bucket, destS3key + '/', promises);
         } else if(filename.endsWith('.xml') || filename.endsWith('.png')) {
             fs.readFile(local_temp_path, function (err, file) {
-              if (err) console.log('readFile err', err); // an error occurred // an error occurred
+              // if (err) console.log('readFile err', err); // an error occurred // an error occurred
               let params = {
                 Bucket: destS3Bucket,
                 Key: destS3key,
@@ -168,7 +166,7 @@ let upload_recursive_dir = function(base_tmpdir, destS3Bucket, s3_key, promises)
 
 let deleteFolderRecursive = function (directory_path) {
     if (fs.existsSync(directory_path)) {
-        console.log('removing folder: ', directory_path);
+        // console.log('removing folder: ', directory_path);
         fs.readdirSync(directory_path).forEach(function (file, index) {
             var currentPath = path.join(directory_path, file);
             if (fs.lstatSync(currentPath).isDirectory()) {
@@ -184,20 +182,20 @@ let deleteFolderRecursive = function (directory_path) {
 /**
  * Sends a response to the pre-signed S3 URL
  */
-let sendResponse = function(callback_url, auth_token, image_number, result) {
+let sendResponse = function(callback_url, auth_token, image_number, result, context) {
 
     const responseBody = JSON.stringify({
-        status: 201,
         number: image_number,
         image_status: result
     });
 
-    console.log('RESPONSE BODY:\n', responseBody);
-    const parsedUrl = url.parse(callback_url);
+    // console.log('RESPONSE BODY:\n', responseBody);
+    const parsedUrl = new URL(callback_url);
+    // console.log('host', parsedUrl.hostname);
     const options = {
         hostname: parsedUrl.hostname,
         port: 443,
-        path: parsedUrl.path,
+        path: '/webhooks/tiled_images',
         method: 'PUT',
         headers: {
             'Content-Type': 'application/json',
@@ -206,17 +204,23 @@ let sendResponse = function(callback_url, auth_token, image_number, result) {
         }
     };
 
-    const req = https.request(options, (res) => {
-        console.log('STATUS:', res.statusCode);
-        console.log('HEADERS:', JSON.stringify(res.headers));
-        callback(null, 'Successfully sent stack response!');
-    });
+    // console.log('options', options);
+    let body='';
 
-    req.on('error', (err) => {
-        console.log('sendResponse Error:\n', err);
-        callback(err);
+    let reqPost =  https.request(options, function(res) {
+        console.log("successful statusCode: ", res.statusCode);
+        res.on('data', function (chunk) {
+            body += chunk;
+        });
+        res.on('end', function () {
+           // console.log("Result", body.toString());
+           context.succeed("Sucess")
+        });
+        res.on('error', function () {
+          // console.log("Result Error", body.toString());
+          context.done(null, 'FAILURE');
+        });
     });
-
-    req.write(responseBody);
-    req.end();
+    reqPost.write(responseBody);
+    reqPost.end();
 };
