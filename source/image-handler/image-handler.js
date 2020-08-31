698e4eab4da2fbd4bb08b9232c881a8feb8b0b66
Alireza Assadzadeh <ssdzd@amazon.com>
2020-08-31 13:47:09 -0400

    Update to re-add files accidentally deleted

---
+++ source/image-handler/image-handler.js
@@ -0,0 +1,241 @@
+/*********************************************************************************************************************
+ *  Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.                                           *
+ *                                                                                                                    *
+ *  Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance    *
+ *  with the License. A copy of the License is located at                                                             *
+ *                                                                                                                    *
+ *      http://www.apache.org/licenses/LICENSE-2.0                                                                    *
+ *                                                                                                                    *
+ *  or in the 'license' file accompanying this file. This file is distributed on an 'AS IS' BASIS, WITHOUT WARRANTIES *
+ *  OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions    *
+ *  and limitations under the License.                                                                                *
+ *********************************************************************************************************************/
+
+const AWS = require('aws-sdk');
+const sharp = require('sharp');
+
+class ImageHandler {
+
+    /**
+     * Main method for processing image requests and outputting modified images.
+     * @param {ImageRequest} request - An ImageRequest object.
+     */
+    async process(request) {
+        const originalImage = request.originalImage;
+        const edits = request.edits;
+        if (edits !== undefined) {
+            const modifiedImage = await this.applyEdits(originalImage, edits);
+            if (request.outputFormat !== undefined) {
+                modifiedImage.toFormat(request.outputFormat);
+            }
+            const bufferImage = await modifiedImage.toBuffer();
+            return bufferImage.toString('base64');
+        } else {
+            return originalImage.toString('base64');
+        }
+    }
+
+    /**
+     * Applies image modifications to the original image based on edits
+     * specified in the ImageRequest.
+     * @param {Buffer} originalImage - The original image.
+     * @param {object} edits - The edits to be made to the original image.
+     */
+    async applyEdits(originalImage, edits) {
+        if (edits.resize === undefined) {
+            edits.resize = {};
+            edits.resize.fit = 'inside';
+        }
+
+        const image = sharp(originalImage, { failOnError: false });
+        const metadata = await image.metadata();
+
+        // Apply the image edits
+        for (const editKey in edits) {
+            const value = edits[editKey];
+            if (editKey === 'overlayWith') {
+                let imageMetadata = metadata;
+                if (edits.resize) {
+                    let imageBuffer = await image.toBuffer();
+                    imageMetadata = await sharp(imageBuffer).resize({ edits: { resize: edits.resize }}).metadata();
+                }
+
+                const { bucket, key, wRatio, hRatio, alpha } = value;
+                const overlay = await this.getOverlayImage(bucket, key, wRatio, hRatio, alpha, imageMetadata);
+                const overlayMetadata = await sharp(overlay).metadata();
+
+                let { options } = value;
+                if (options) {
+                    if (options.left !== undefined) {
+                        let left = options.left;
+                        if (isNaN(left) && left.endsWith('p')) {
+                            left = parseInt(left.replace('p', ''));
+                            if (left < 0) {
+                                left = imageMetadata.width + (imageMetadata.width * left / 100) - overlayMetadata.width;
+                            } else {
+                                left = imageMetadata.width * left / 100;
+                            }
+                        } else {
+                            left = parseInt(left);
+                            if (left < 0) {
+                                left = imageMetadata.width + left - overlayMetadata.width;
+                            }
+                        }
+                        isNaN(left) ? delete options.left : options.left = left;
+                    }
+                    if (options.top !== undefined) {
+                        let top = options.top;
+                        if (isNaN(top) && top.endsWith('p')) {
+                            top = parseInt(top.replace('p', ''));
+                            if (top < 0) {
+                                top = imageMetadata.height + (imageMetadata.height * top / 100) - overlayMetadata.height;
+                            } else {
+                                top = imageMetadata.height * top / 100;
+                            }
+                        } else {
+                            top = parseInt(top);
+                            if (top < 0) {
+                                top = imageMetadata.height + top - overlayMetadata.height;
+                            }
+                        }
+                        isNaN(top) ? delete options.top : options.top = top;
+                    }
+                }
+
+                const params = [{ ...options, input: overlay }];
+                image.composite(params);
+            } else if (editKey === 'smartCrop') {
+                const options = value;
+                const imageBuffer = await image.toBuffer();
+                const boundingBox = await this.getBoundingBox(imageBuffer, options.faceIndex);
+                const cropArea = this.getCropArea(boundingBox, options, metadata);
+                try {
+                    image.extract(cropArea)
+                } catch (err) {
+                    throw ({
+                        status: 400,
+                        code: 'SmartCrop::PaddingOutOfBounds',
+                        message: 'The padding value you provided exceeds the boundaries of the original image. Please try choosing a smaller value or applying padding via Sharp for greater specificity.'
+                    });
+                }
+            } else {
+                image[editKey](value);
+            }
+        }
+        // Return the modified image
+        return image;
+    }
+
+    /**
+     * Gets an image to be used as an overlay to the primary image from an
+     * Amazon S3 bucket.
+     * @param {string} bucket - The name of the bucket containing the overlay.
+     * @param {string} key - The object keyname corresponding to the overlay.
+     * @param {number} wRatio - The width rate of the overlay image.
+     * @param {number} hRatio - The height rate of the overlay image.
+     * @param {number} alpha - The transparency alpha to the overlay.
+     * @param {object} sourceImageMetadata - The metadata of the source image.
+     */
+    async getOverlayImage(bucket, key, wRatio, hRatio, alpha, sourceImageMetadata) {
+        const s3 = new AWS.S3();
+        const params = { Bucket: bucket, Key: key };
+        try {
+            const { width, height } = sourceImageMetadata;
+            const overlayImage = await s3.getObject(params).promise();
+            let resize = {
+                fit: 'inside'
+            }
+
+            // Set width and height of the watermark image based on the ratio
+            const zeroToHundred = /^(100|[1-9]?[0-9])$/;
+            if (zeroToHundred.test(wRatio)) {
+                resize['width'] = parseInt(width * wRatio / 100);
+            }
+            if (zeroToHundred.test(hRatio)) {
+                resize['height'] = parseInt(height * hRatio / 100);
+            }
+
+            // If alpha is not within 0-100, the default alpha is 0 (fully opaque).
+            if (zeroToHundred.test(alpha)) {
+                alpha = parseInt(alpha);
+            } else {
+                alpha = 0;
+            }
+
+            const convertedImage = await sharp(overlayImage.Body)
+                .resize(resize)
+                .composite([{
+                    input: Buffer.from([255, 255, 255, 255 * (1 - alpha / 100)]),
+                    raw: {
+                        width: 1,
+                        height: 1,
+                        channels: 4
+                    },
+                    tile: true,
+                    blend: 'dest-in'
+                }]).toBuffer();
+            return convertedImage;
+        } catch (err) {
+            throw {
+                status: err.statusCode ? err.statusCode : 500,
+                code: err.code,
+                message: err.message
+            };
+        }
+    }
+
+    /**
+     * Calculates the crop area for a smart-cropped image based on the bounding
+     * box data returned by Amazon Rekognition, as well as padding options and
+     * the image metadata.
+     * @param {Object} boundingBox - The boudning box of the detected face.
+     * @param {Object} options - Set of options for smart cropping.
+     * @param {Object} metadata - Sharp image metadata.
+     */
+    getCropArea(boundingBox, options, metadata) {
+        const padding = (options.padding !== undefined) ? parseFloat(options.padding) : 0;
+        // Calculate the smart crop area
+        const cropArea = {
+            left : parseInt((boundingBox.Left*metadata.width)-padding),
+            top : parseInt((boundingBox.Top*metadata.height)-padding),
+            width : parseInt((boundingBox.Width*metadata.width)+(padding*2)),
+            height : parseInt((boundingBox.Height*metadata.height)+(padding*2)),
+        }
+        // Return the crop area
+        return cropArea;
+    }
+
+    /**
+     * Gets the bounding box of the specified face index within an image, if specified.
+     * @param {Sharp} imageBuffer - The original image.
+     * @param {Integer} faceIndex - The zero-based face index value, moving from 0 and up as
+     * confidence decreases for detected faces within the image.
+     */
+    async getBoundingBox(imageBuffer, faceIndex) {
+        const rekognition = new AWS.Rekognition();
+        const params = { Image: { Bytes: imageBuffer }};
+        const faceIdx = (faceIndex !== undefined) ? faceIndex : 0;
+        try {
+            const response = await rekognition.detectFaces(params).promise();
+            return response.FaceDetails[faceIdx].BoundingBox;
+        } catch (err) {
+            console.log(err);
+            if (err.message === "Cannot read property 'BoundingBox' of undefined") {
+                throw {
+                    status: 400,
+                    code: 'SmartCrop::FaceIndexOutOfRange',
+                    message: 'You have provided a FaceIndex value that exceeds the length of the zero-based detectedFaces array. Please specify a value that is in-range.'
+                };
+            } else {
+                throw {
+                    status: 500,
+                    code: err.code,
+                    message: err.message
+                };
+            }
+        }
+    }
+}
+
+// Exports
+module.exports = ImageHandler;
---
+++ source/image-handler/thumbor-mapping.js
@@ -0,0 +1,263 @@
+/*********************************************************************************************************************
+ *  Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.                                           *
+ *                                                                                                                    *
+ *  Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance    *
+ *  with the License. A copy of the License is located at                                                             *
+ *                                                                                                                    *
+ *      http://www.apache.org/licenses/LICENSE-2.0                                                                    *
+ *                                                                                                                    *
+ *  or in the 'license' file accompanying this file. This file is distributed on an 'AS IS' BASIS, WITHOUT WARRANTIES *
+ *  OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions    *
+ *  and limitations under the License.                                                                                *
+ *********************************************************************************************************************/
+
+const Color = require('color');
+const ColorName = require('color-name');
+
+class ThumborMapping {
+
+    // Constructor
+    constructor() {
+        this.edits = {};
+    }
+
+    /**
+     * Initializer function for creating a new Thumbor mapping, used by the image
+     * handler to perform image modifications based on legacy URL path requests.
+     * @param {object} event - The request body.
+     */
+    process(event) {
+        // Setup
+        this.path = event.path;
+        const edits = this.path.split('/');
+        const filetype = (this.path.split('.'))[(this.path.split('.')).length - 1];
+
+        // Process the Dimensions
+        const dimPath = this.path.match(/[^\/](\d+x\d+)|(0x\d+)/g);
+        if (dimPath) {
+            // Assign dimenions from the first match only to avoid parsing dimension from image file names
+            const dims = dimPath[0].split('x');
+            const width = Number(dims[0]);
+            const height = Number(dims[1]);
+
+            // Set only if the dimensions provided are valid
+            if (!isNaN(width) && !isNaN(height)) {
+                this.edits.resize = {};
+
+                // If width or height is 0, fit would be inside.
+                if (width === 0 || height === 0) {
+                    this.edits.resize.fit = 'inside';
+                }
+                this.edits.resize.width = width === 0 ? null : width;
+                this.edits.resize.height = height === 0 ? null : height;
+            }
+        }
+
+        // Parse the image path
+        for (let i = 0; i < edits.length; i++) {
+            const edit = edits[i];
+            if (edit === ('fit-in')) {
+                if (this.edits.resize === undefined) {
+                    this.edits.resize = {};
+                }
+
+                this.edits.resize.fit = 'inside';
+            } else if (edit.includes('filters:')) {
+                this.mapFilter(edit, filetype);
+            }
+        }
+
+        return this;
+    }
+
+    /**
+     * Enables users to migrate their current image request model to the SIH solution,
+     * without changing their legacy application code to accomodate new image requests.
+     * @param {string} path - The URL path extracted from the web request.
+     */
+    parseCustomPath(path) {
+        // Setup from the environment variables
+        const matchPattern = process.env.REWRITE_MATCH_PATTERN;
+        const substitution = process.env.REWRITE_SUBSTITUTION;
+        // Perform the substitution and return
+        if (path !== undefined && matchPattern !== undefined && substitution !== undefined) {
+            const parsedPath = path.replace(matchPattern, substitution);
+            const output = { path: parsedPath };
+            return output;
+        } else {
+            throw new Error('ThumborMapping::ParseCustomPath::ParsingError');
+        }
+    }
+
+    /**
+     * Scanner function for matching supported Thumbor filters and converting their
+     * capabilities into Sharp.js supported operations.
+     * @param {string} edit - The URL path filter.
+     * @param {string} filetype - The file type of the original image.
+     */
+    mapFilter(edit, filetype) {
+        const matched = edit.match(/:(.+)\((.*)\)/);
+        const editKey = matched[1];
+        let value = matched[2];
+        // Find the proper filter
+        if (editKey === ('autojpg')) {
+            this.edits.toFormat = 'jpeg';
+        }
+        else if (editKey === ('background_color')) {
+            if (!ColorName[value]) {
+                value = `#${value}`
+            }
+            this.edits.flatten = { background: Color(value).object() };
+        }
+        else if (editKey === ('blur')) {
+            const val = value.split(',');
+            this.edits.blur = (val.length > 1) ? Number(val[1]) : Number(val[0]) / 2;
+        }
+        else if (editKey === ('convolution')) {
+            const arr = value.split(',');
+            const strMatrix = (arr[0]).split(';');
+            let matrix = [];
+            strMatrix.forEach(function(str) {
+                matrix.push(Number(str));
+            });
+            const matrixWidth = arr[1];
+            let matrixHeight = 0;
+            let counter = 0;
+            for (let i = 0; i < matrix.length; i++) {
+                if (counter === (matrixWidth - 1)) {
+                    matrixHeight++;
+                    counter = 0;
+                } else {
+                    counter++;
+                }
+            }
+            this.edits.convolve = {
+                width: Number(matrixWidth),
+                height: Number(matrixHeight),
+                kernel: matrix
+            }
+        }
+        else if (editKey === ('equalize')) {
+            this.edits.normalize = "true";
+        }
+        else if (editKey === ('fill')) {
+            if (this.edits.resize === undefined) {
+                this.edits.resize = {};
+            }
+            if (!ColorName[value]) {
+                value = `#${value}`
+            }
+            this.edits.resize.fit = 'contain';
+            this.edits.resize.background = Color(value).object();
+        }
+        else if (editKey === ('format')) {
+            const formattedValue = value.replace(/[^0-9a-z]/gi, '').replace(/jpg/i, 'jpeg');
+            const acceptedValues = ['heic', 'heif', 'jpeg', 'png', 'raw', 'tiff', 'webp'];
+            if (acceptedValues.includes(formattedValue)) {
+                this.edits.toFormat = formattedValue;
+            }
+        }
+        else if (editKey === ('grayscale')) {
+            this.edits.grayscale = true;
+        }
+        else if (editKey === ('no_upscale')) {
+            if (this.edits.resize === undefined) {
+                this.edits.resize = {};
+            }
+            this.edits.resize.withoutEnlargement = true;
+        }
+        else if (editKey === ('proportion')) {
+            if (this.edits.resize === undefined) {
+                this.edits.resize = {};
+            }
+            const prop = Number(value);
+            this.edits.resize.width = Number(this.edits.resize.width * prop);
+            this.edits.resize.height = Number(this.edits.resize.height * prop);
+        }
+        else if (editKey === ('quality')) {
+            if (['jpg', 'jpeg'].includes(filetype)) {
+                this.edits.jpeg = { quality: Number(value) }
+            } else if (filetype === 'png') {
+                this.edits.png = { quality: Number(value) }
+            } else if (filetype === 'webp') {
+                this.edits.webp = { quality: Number(value) }
+            } else if (filetype === 'tiff') {
+                this.edits.tiff = { quality: Number(value) }
+            } else if (filetype === 'heif') {
+                this.edits.heif = { quality: Number(value) }
+            }
+        }
+        else if (editKey === ('rgb')) {
+            const percentages = value.split(',');
+            const values = [];
+            percentages.forEach(function (percentage) {
+                const parsedPercentage = Number(percentage);
+                const val = 255 * (parsedPercentage / 100);
+                values.push(val);
+            })
+            this.edits.tint = { r: values[0], g: values[1], b: values[2] };
+        }
+        else if (editKey === ('rotate')) {
+            this.edits.rotate = Number(value);
+        }
+        else if (editKey === ('sharpen')) {
+            const sh = value.split(',');
+            const sigma = 1 + Number(sh[1]) / 2;
+            this.edits.sharpen = sigma;
+        }
+        else if (editKey === ('stretch')) {
+            if (this.edits.resize === undefined) {
+                this.edits.resize = {};
+            }
+
+            // If fit-in is not defined, fit parameter would be 'fill'.
+            if (this.edits.resize.fit !== 'inside') {
+                this.edits.resize.fit = 'fill';
+            }
+        }
+        else if (editKey === ('strip_exif')) {
+            this.edits.rotate = 0;
+        }
+        else if (editKey === ('strip_icc')) {
+            this.edits.rotate = 0;
+        }
+        else if (editKey === ('upscale')) {
+            if (this.edits.resize === undefined) {
+                this.edits.resize = {};
+            }
+            this.edits.resize.fit = "inside"
+        }
+        else if (editKey === ('watermark')) {
+            const options = value.replace(/\s+/g, '').split(',');
+            const bucket = options[0];
+            const key = options[1];
+            const xPos = options[2];
+            const yPos = options[3];
+            const alpha = options[4];
+            const wRatio = options[5];
+            const hRatio = options[6];
+
+            this.edits.overlayWith = {
+                bucket,
+                key,
+                alpha,
+                wRatio,
+                hRatio,
+                options: {}
+            }
+            const allowedPosPattern = /^(100|[1-9]?[0-9]|-(100|[1-9][0-9]?))p$/;
+            if (allowedPosPattern.test(xPos) || !isNaN(xPos)) {
+                this.edits.overlayWith.options['left'] = xPos;
+            }
+            if (allowedPosPattern.test(yPos) || !isNaN(yPos)) {
+                this.edits.overlayWith.options['top'] = yPos;
+            }
+        }
+        else {
+            return undefined;
+        }
+    }
+}
+
+// Exports
+module.exports = ThumborMapping;
