710aa84f091dc75b1d5acf772bf5f5d2424402b8
Mike Bradford <48229313+mbradford-verys@users.noreply.github.com>
2021-03-25 15:21:35 -0700

    delete unused stuff and simplify repo structure

--- source/constructs/bin/constructs.ts
+++
@@ -1,8 +0,0 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-import * as cdk from '@aws-cdk/core';
-import { ConstructsStack } from '../lib/constructs-stack';
-
-const app = new cdk.App();
-new ConstructsStack(app, 'ConstructsStack');
