22489345c5641901bf6d39b8aec9d0a4c617f808
Charles Wei <wch@amazon.com>
2021-07-09 15:09:38 +0800

    chore: add inline doc

--- source/ecs-image-handler/src/store.ts
+++ source/ecs-image-handler/src/store.ts
@@ -3,12 +3,28 @@ import * as path from 'path';
 import * as S3 from 'aws-sdk/clients/s3';
 import * as HttpErrors from 'http-errors';
 
-
+/**
+ * A abstract store to get file data.
+ * It can either get from s3 or local filesystem.
+ */
 export interface IStore {
+
+  /**
+   * Read all buffer from underlying.
+   * Return both the buffer and the s3 object/file type.
+   * Usually the file type is the file's suffix.
+   *
+   * @param p the path of the s3 object or the file
+   * @param shortcut if true, S3Store will raise HTTP 403 error to CloudFront to tell it directly access s3 object.
+   * This is intended to bypass ECS for the scenario reading original image from s3 without modification.
+   */
   get(p: string, shortcut?: boolean): Promise<{ buffer: Buffer; type: string }>;
 }
 
 
+/**
+ * A local file system based store.
+ */
 export class LocalStore implements IStore {
   public constructor(private root: string = '') {}
   public async get(p: string, _?: boolean): Promise<{ buffer: Buffer; type: string }> {
@@ -20,7 +36,9 @@ export class LocalStore implements IStore {
   }
 }
 
-
+/**
+ * S3 based store.
+ */
 export class S3Store implements IStore {
   private _s3: S3 = new S3();
   public constructor(public readonly bucket: string) {}
@@ -43,7 +61,9 @@ export class S3Store implements IStore {
   }
 }
 
-
+/**
+ * A fake store. Only for unit test.
+ */
 export class NullStore implements IStore {
   public async get(p: string, _?: boolean): Promise<{ buffer: Buffer; type: string }> {
     return Promise.resolve({
