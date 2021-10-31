aee1e3d0c1f8af8f00f22ea819dd39941b7d7a4d
wchaws <javacs3+aws@gmail.com>
2021-10-31 12:47:58 +0000

    fix: constructs test

--- source/constructs/test/constructs.test.ts
+++ source/constructs/test/constructs.test.ts
@@ -1,11 +1,8 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
 import * as Constructs from '../lib/constructs-stack';
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-import TestTemplate = require('./serverless-image-handler-test.json');
 
 
 test('Serverless Image Handler Stack', () => {
@@ -13,5 +10,6 @@ test('Serverless Image Handler Stack', () => {
   // WHEN
   const stack = new Constructs.ConstructsStack(app, 'MyTestStack');
   // THEN
-  expectCDK(stack).to(matchTemplate(TestTemplate, MatchStyle.EXACT));
+
+  expect(app.synth().getStackArtifact(stack.artifactId).template).toMatchSnapshot();
 });
