2d6054fc3111aaa8bbe24e5491380b7e4b76db5b
Ernesto Rodriguez Ortiz <ernesto.ortiz@tracktik.com>
2022-04-18 15:05:34 -0400

    feat: set timeout and retry options for s3 client

--- source/image-handler/index.ts
+++ source/image-handler/index.ts
@@ -13,7 +13,21 @@ import { Headers, ImageHandlerEvent, ImageHandlerExecutionResult, StatusCodes } 
 import { SecretProvider } from "./secret-provider";
 
 const awsSdkOptions = getOptions();
-const s3Client = new S3(awsSdkOptions);
+const s3Client = new S3({
+  ...awsSdkOptions,
+  maxRetries: 3,
+  retryDelayOptions: {
+    customBackoff: (retryCount) => {
+      const wait = 2 ** retryCount * 100;
+      console.log(`retry count: ${retryCount}, waiting: ${wait}ms`);
+      return wait; // in milliseconds
+    },
+  },
+  httpOptions: {
+    connectTimeout: 1000, // in milliseconds
+    timeout: 5000, // in milliseconds
+  },
+});
 const rekognitionClient = new Rekognition(awsSdkOptions);
 const secretsManagerClient = new SecretsManager(awsSdkOptions);
 const secretProvider = new SecretProvider(secretsManagerClient);
