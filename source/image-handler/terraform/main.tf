1f7fb9b01d531a15575b175cc5ec1e06d28e8c98
Moritz Zimmer <moritz.zmmr@gmail.com>
2022-04-29 13:41:52 +0200

    refactor: use new log streaming lambda
    [skip ci]

--- source/image-handler/terraform/main.tf
+++ source/image-handler/terraform/main.tf
@@ -63,7 +63,7 @@ module "lambda" {
   }
 
   cloudwatch_log_subscription_filters = {
-    elasticsearch = {
+    opensearch = {
       destination_arn = data.aws_lambda_function.log_streaming.arn
     }
   }
