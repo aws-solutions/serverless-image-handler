aeb0d01557ef1abc5de769d0437a17c514444e36
Garvit Singh <garvits@amazon.com>
2023-01-20 15:04:04 -0500

    Refactor build script

--- deployment/build-s3-dist.sh
+++ deployment/build-s3-dist.sh
@@ -6,15 +6,16 @@
 # cd deployment
 # ./build-s3-dist.sh source-bucket-base-name trademarked-solution-name version-code
 #
-# Paramenters:
+# For example: ./build-s3-dist.sh solutions my-solution v1.0.0
+# Parameters:
 #  - source-bucket-base-name: Name for the S3 bucket location where the template will source the Lambda
 #    code from. The template will append '-[region_name]' to this bucket name.
-#    For example: ./build-s3-dist.sh solutions my-solution v1.0.0
 #    The template will then expect the source code to be located in the solutions-[region_name] bucket
-#
 #  - trademarked-solution-name: name of the solution for consistency
-#
 #  - version-code: version of the package
+
+[ "$DEBUG" == 'true' ] && set -x
+set -e
 
 # Check to see if input has been provided:
 if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
@@ -23,78 +24,48 @@ if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
     exit 1
 fi
 
-# Exit immediately if a command exits with a non-zero status.
-set -e
+function headline(){
+  echo "------------------------------------------------------------------------------"
+  echo "$1"
+  echo "------------------------------------------------------------------------------"
+}
 
-# Get reference for all important folders
+headline "[Init] Setting up paths"
 template_dir="$PWD"
 template_dist_dir="$template_dir/global-s3-assets"
 build_dist_dir="$template_dir/regional-s3-assets"
 source_dir="$template_dir/../source"
 cdk_source_dir="$source_dir/constructs"
 
-echo "------------------------------------------------------------------------------"
-echo "[Init] Clean old dist folders"
-echo "------------------------------------------------------------------------------"
-echo "rm -rf $template_dist_dir"
-rm -rf $template_dist_dir
-echo "mkdir -p $template_dist_dir"
-mkdir -p $template_dist_dir
-echo "rm -rf $build_dist_dir"
-rm -rf $build_dist_dir
-echo "mkdir -p $build_dist_dir"
-mkdir -p $build_dist_dir
+headline "[Init] Clean old folders"
+rm -rf "$template_dist_dir"
+mkdir -p "$template_dist_dir"
+rm -rf "$build_dist_dir"
+mkdir -p "$build_dist_dir"
 
-echo "------------------------------------------------------------------------------"
-echo "Synthesize the CDK project into a CloudFormation template"
-echo "------------------------------------------------------------------------------"
+headline "[Package] CDK project into a CloudFormation template"
 export SOLUTION_BUCKET_NAME_PLACEHOLDER=$1
 export SOLUTION_NAME_PLACEHOLDER=$2
 export SOLUTION_VERSION_PLACEHOLDER=$3
-export overrideWarningsEnabled=false
 
-cd $cdk_source_dir
-npm run clean
-npm install
-node_modules/aws-cdk/bin/cdk synth --asset-metadata false --path-metadata false >$template_dist_dir/$2.template
+cd "$cdk_source_dir"
+npm run clean:install
+overrideWarningsEnabled=false npx cdk synth --asset-metadata false --path-metadata false --json false>"$template_dist_dir"/"$2".template
 
-declare -a lambda_packages=(
-    "image-handler"
-    "custom-resource"
-)
+headline "[Package] Lambda binaries and copy to build_dist_dir"
+cd "$source_dir"
+npm run build:all
+find . -mindepth 1 -maxdepth 3 -type f -iname "*.zip" -exec cp {} "$build_dist_dir" \;
 
-for lambda_package in "${lambda_packages[@]}"; do
-    echo "------------------------------------------------------------------------------"
-    echo "Building Lambda package: $lambda_package"
-    echo "------------------------------------------------------------------------------"
-    cd $source_dir/$lambda_package
-    npm run package
-    # Check the result of the package step and exit if a failure is identified
-    if [ $? -eq 0 ]; then
-        echo "Package for $lambda_package built successfully"
-    else
-        echo "******************************************************************************"
-        echo "Lambda package build FAILED for $lambda_package"
-        echo "******************************************************************************"
-        exit 1
-    fi
-    mv dist/package.zip $build_dist_dir/$lambda_package.zip
-    rm -rf dist
-done
+headline "[Package] Serverless Image Handler Demo UI"
+mkdir "$build_dist_dir"/demo-ui/
+cp -r "$source_dir"/demo-ui/** "$build_dist_dir"/demo-ui/
 
-echo "------------------------------------------------------------------------------"
-echo "Package Serverless Image Handler Demo UI"
-echo "------------------------------------------------------------------------------"
-mkdir $build_dist_dir/demo-ui/
-cp -r $source_dir/demo-ui/** $build_dist_dir/demo-ui/
-
-echo "------------------------------------------------------------------------------"
-echo "[Create] Console manifest"
-echo "------------------------------------------------------------------------------"
-cd $source_dir/demo-ui
+headline "[Package] Console manifest"
+cd "$source_dir"/demo-ui
 manifest=($(find * -type f ! -iname ".DS_Store"))
 manifest_json=$(
     IFS=,
     printf "%s" "${manifest[*]}"
 )
-echo "{\"files\":[\"$manifest_json\"]}" | sed 's/,/","/g' >>$build_dist_dir/demo-ui-manifest.json
+echo "{\"files\":[\"$manifest_json\"]}" | sed 's/,/","/g' >>"$build_dist_dir"/demo-ui-manifest.json
