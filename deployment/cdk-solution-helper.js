4799229ee6df733c60db74e65923252e316e41ba
Garvit Singh <garvits@amazon.com>
2023-01-25 16:39:01 -0500

    Cleanup run-unit-test script and placeholder file for cdk solution helper

---
+++ deployment/cdk-solution-helper.js
@@ -0,0 +1,6 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+// TODO update references in generated template for lambda binaries
+//  to solution buckets
+
