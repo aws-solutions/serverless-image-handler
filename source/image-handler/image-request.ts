f02a7a6c0fb045ab0b58c9ef660300c6fd7ef2cc
Siyanatullah Khan <siyanatullah.khan@stocktwits.com>
2023-06-06 11:14:26 +0530

    [GROWTH-1032] space fix

--- source/image-handler/image-request.ts
+++ source/image-handler/image-request.ts
@@ -27,7 +27,6 @@ import { URL } from "url";
 const MAX_PERCENTAGE = parseInt(process.env.MAX_PERCENTAGE,10) || 75
 const MIN_PERCENTAGE = parseInt(process.env.MIN_PERCENTAGE, 10) || 25
 const GIF_ALLOWED_RESIZE = parseInt(process.env.GIF_ALLOWED_RESIZE,10) || 4 * 1024 * 1024 
-
 const MAX_IMAGE_SIZE = 6 * 1024 * 1024; //6 MB
 const ALLOWED_CONTENT_TYPES = [
   'image/jpeg',
@@ -125,14 +124,12 @@ export class ImageRequest {
       imageRequestInfo.edits = this.parseImageEdits(event, imageRequestInfo.requestType);
 
       const originalImage = await this.getOriginalImage(imageRequestInfo.bucket, imageRequestInfo.key);
-  
       imageRequestInfo = { ...imageRequestInfo, ...originalImage };
 
       imageRequestInfo.headers = this.parseImageHeaders(event, imageRequestInfo.requestType);
 
       await this.setResizeDimensionsforGifIfRequired(originalImage,imageRequestInfo);
 
-    
       // If the original image is SVG file and it has any edits but no output format, change the format to PNG.
       if (
         imageRequestInfo.contentType === ContentTypes.SVG &&
