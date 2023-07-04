e4529ed04ff1a94a3ffc1e9fdf8461bb37b48ddc
Barnaby Mercer <barnaby@mesomorphic.co.uk>
2023-07-04 12:49:26 +0100

    Update infer-image-type.spec.ts
    Generalise tests for other magic numbers

--- source/image-handler/test/image-request/infer-image-type.spec.ts
+++ source/image-handler/test/image-request/infer-image-type.spec.ts
@@ -12,6 +12,20 @@ describe("inferImageType", () => {
   const secretsManager = new SecretsManager();
   const secretProvider = new SecretProvider(secretsManager);
 
+  test.each([
+    { value: [0xff, 0xd8, 0xff, 0xee, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00], label: "FFD8FFEE" },
+    { value: [0xff, 0xd8, 0xff, 0xe2, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00], label: "FFD8FFE2" },
+  ])('Should pass if it returns "image/jpeg" for a magic number of $label', ({ value, label }) => {
+    const imageBuffer = Buffer.from(value);
+
+    // Act
+    const imageRequest = new ImageRequest(s3Client, secretProvider);
+    const result = imageRequest.inferImageType(imageBuffer);
+
+    // Assert
+    expect(result).toEqual("image/jpeg");
+  });
+
   it('Should pass if it returns "image/jpeg"', () => {
     // Arrange
     const imageBuffer = Buffer.from([0xff, 0xd8, 0xff, 0xee, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]);
