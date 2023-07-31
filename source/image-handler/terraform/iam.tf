8d3d873be4117560c81fbcad98f6313ac296571f
Matthias Naber <naber.matthias+github@gmail.com>
2023-07-31 15:48:39 +0200

    fix: added missing iam permissions for detecting http/404
    [skip ci]

--- source/image-handler/terraform/iam.tf
+++ source/image-handler/terraform/iam.tf
@@ -3,6 +3,10 @@ data "aws_iam_policy_document" "lambda" {
     actions   = ["s3:GetObject"]
     resources = ["${aws_s3_bucket.images.arn}/*"]
   }
+  statement {
+    actions   = ["s3:ListBucket"]
+    resources = [aws_s3_bucket.images.arn]
+  }
 }
 
 resource "aws_iam_policy" "lambda" {
